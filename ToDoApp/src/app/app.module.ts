7b1ad1dd692df7fce26fb8ffc9aab0492f88f603
JanSystemic <just4fan.jl@gmail.com>
2018-04-28 09:33:47 +0300

    start app

--- ToDoApp/src/app/app.module.ts
+++ ToDoApp/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {TodoService} from './taskdialog/to-do.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TaskdialogComponent } from './taskdialog/taskdialog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskdialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
